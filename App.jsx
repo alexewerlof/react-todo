37dba9a0124d0792517e2d2a8b2a747118f45763
Alex Ewerlöf <Alex Ewerlf>
2016-06-21 01:11:09 +0200

    after lesson 7

--- App.jsx
+++ App.jsx
@@ -4,9 +4,33 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
   constructor() {
     super();
+    this.state = {
+      red: 0,
+      green: 0,
+      blue: 0
+    }
+    this.update = this.update.bind(this);
+  }
+  update(e) {
+    this.setState({
+      red: ReactDOM.findDOMNode(this.refs.red).value,
+      green: ReactDOM.findDOMNode(this.refs.green).value,
+      blue: ReactDOM.findDOMNode(this.refs.blue).value,
+    });
   }
   render() {
-    return <div>Hello {this.props.txt}!</div>
+    var bg = {
+      background: 'rgb(' + this.state.red + ', ' + this.state.green + ', ' + this.state.blue + ')'
+    };
+    return (
+      <div style={bg}>
+        {this.state.red}, {this.state.green}, {this.state.blue}
+        <br/>
+        <Slider ref="red" update={this.update}></Slider>
+        <Slider ref="green" update={this.update}></Slider>
+        <Slider ref="blue" update={this.update}></Slider>
+      </div>
+    )
   }
 }
 
@@ -17,6 +41,12 @@ App.propTypes = {
 
 App.defaultProps = {
   txt: 'default text'
+}
+
+class Slider extends React.Component {
+  render() {
+    return <input type="range" min="0" max="255" onChange={this.props.update} />;
+  }
 }
 
 export default App;
