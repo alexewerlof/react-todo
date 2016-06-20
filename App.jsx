7cde8455379efa26cc993084ea96e53f5cab7d1f
Alex Ewerlöf <Alex Ewerlf>
2016-06-21 00:14:47 +0200

    after step 4 of react fundamentals

--- App.jsx
+++ App.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+  constructor() {
+    super();
+  }
   render() {
-    return <div>Hello</div>
+    return <div>Hello {this.props.txt}!</div>
   }
 }
 
+App.propTypes = {
+  txt: React.PropTypes.string,
+  cat: React.PropTypes.number.isRequired
+}
+
+App.defaultProps = {
+  txt: 'default text'
+}
+
 export default App;
+
+ReactDOM.render(<App txt="this is the prop text 8" cat={5} />, document.getElementById('app'));
