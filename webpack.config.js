9b0f28b73680b2325cd6ed021f208f91f16e1976
Alex Ewerlöf <Alex Ewerlf>
2016-06-20 19:34:33 +0200

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  entry: './main.jsx',
+  output: {
+    path: './',
+    filename: 'index.js'
+  },
+  devServer: {
+    inline: true,
+    port: 3333
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  }
+};
