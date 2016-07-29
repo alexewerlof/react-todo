bfac50189aef0954e441a5c25dda86db055dba51
userpixel <hanifbbz@gmail.com>
2016-07-29 19:27:44 +0200

    small syntax improvements

--- App.jsx
+++ App.jsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import Todo from './Todo.jsx'
 
-export default class App extends React.Component {
-  render() {
-    console.log(typeof Todo + Todo)
-    console.dir(Todo)
-    return <Todo />;
-  }
+export default function App () {
+  return <Todo />;
 }
