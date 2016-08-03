83753380defcc55f6bc12d15d958f6210bf9b1dc
userpixel <hanifbbz@gmail.com>
2016-08-03 21:43:08 +0200

    run all js files through babel

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,14 @@ module.exports = {
         query: {
           presets: ['es2015', 'react']
         }
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015']
+        }
       }
     ]
   }
