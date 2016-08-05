88155f9e43d19175014ae06d71adfbc27dbd1cdb
userpixel <hanifbbz@gmail.com>
2016-08-05 21:14:43 +0200

    reducers and store are separate

--- reducers.js
+++ reducers.js
@@ -1,11 +1,19 @@
 import {Task} from './store';
 import TaskClass from './TaskClass';
+import {combineReducers} from 'redux';
 
-export function setFilter(filter, newFilter) {
+
+// Utility function that makes a new object from the original object with what
+// is modified in what
+function newModifiedObject(originalObject, whatChangedObject) {
+  return Object.assign({}, originalObject, whatChangedObject);
+}
+
+function setFilter(filter, newFilter) {
   return newFilter;
 }
 
-export function taskRemove(tasks, taskId) {
+function taskRemove(tasks, taskId) {
   var index = tasks.findIndex(task => task.id === taskId);
   if (index !== -1) {
     tasks = tasks.slice();
@@ -15,7 +23,7 @@ export function taskRemove(tasks, taskId) {
   return tasks;
 }
 
-export function taskToggle(tasks, taskId) {
+function taskToggle(tasks, taskId) {
   var index = tasks.findIndex(task => task.id === taskId);
   if (index !== -1) {
     tasks = tasks.slice();
@@ -26,8 +34,52 @@ export function taskToggle(tasks, taskId) {
   return tasks;
 }
 
-export function taskNew(tasks, title) {
+function taskNew(tasks, title) {
   var newTasks = tasks.slice();
   newTasks.push(new TaskClass(title));
   return newTasks;
 }
+
+// ---------------------------------- Reducers ---------------------------------
+
+// Reducers for the tasks branch of the state
+function tasks(state = INIT_STORE.tasks, action) {
+  switch (action.type) {
+    case 'TASK-TOGGLE':
+      return taskToggle(state, action.payload);
+    case 'TASK-REMOVE':
+      return taskRemove(state, action.payload);
+    case 'TASK-NEW':
+      return taskNew(state, action.payload);
+    default:
+      return state;
+  }
+}
+
+// Reducers for the tasks branch of the state
+function filter(state = INIT_STORE.filter, action) {
+  switch (action.type) {
+    case 'SET_FILTER':
+      return setFilter(state, action.payload);
+    default:
+      return state;
+  }
+}
+
+// ------------------------------ Root Reducer ---------------------------------
+
+const INIT_STORE = {
+  tasks: [
+    new TaskClass('milk the caws who are awesome'),
+    new TaskClass('call doctor', true),
+    new TaskClass('buy tickets')
+  ],
+  filter: 'all'
+};
+
+var rootReducer = combineReducers({
+  tasks,
+  filter
+});
+
+export default rootReducer;
--- store.js
+++ store.js
@@ -1,36 +1,6 @@
 import {createStore} from 'redux';
-import {setFilter, taskRemove, taskToggle, taskNew} from './reducers';
-import TaskClass from './TaskClass';
+import rootReducer from './reducers';
 
-const INIT_STORE = {
-  tasks: [
-    new TaskClass('milk the caws who are awesome'),
-    new TaskClass('call doctor', true),
-    new TaskClass('buy tickets')
-  ],
-  filter: 'all'
-};
-
-// Makes a new object from the original object with what is modified in what
-function newModifiedObject(originalObject, whatChangedObject) {
-  return Object.assign({}, originalObject, whatChangedObject);
-}
-
-function reducer (state = INIT_STORE, action) {
-  switch (action.type) {
-    case 'SET_FILTER':
-      return newModifiedObject(state, {filter: setFilter(state.filter, action.payload)});
-    case 'TASK-TOGGLE':
-      return newModifiedObject(state, {tasks: taskToggle(state.tasks, action.payload)});
-    case 'TASK-REMOVE':
-      return newModifiedObject(state, {tasks: taskRemove(state.tasks, action.payload)});
-    case 'TASK-NEW':
-      return newModifiedObject(state, {tasks: taskNew(state.tasks, action.payload)});
-    default:
-      return state;
-  }
-}
-
-const store = createStore(reducer, window.devToolsExtension && window.devToolsExtension());
+const store = createStore(rootReducer, window.devToolsExtension && window.devToolsExtension());
 
 export default store;
